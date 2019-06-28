07a274887f9d22845c2981e13121dc12e0f1b840
Amaresh Prasad Nayak <AN00533009@techmahindra.com>
2019-06-28 17:18:24 +0530

    DELETED

--- bluebird/ServiceLib/obj/Debug/generated/src/Com.Morpho.Android.Annotation.WakeLockabbleAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Com.Morpho.Android.Annotation {
-
-	[global::Android.Runtime.Annotation ("com.morpho.android.annotation.WakeLockabble")]
-	public partial class WakeLockabbleAttribute : Attribute
-	{
-	}
-}
--- bluebird/SwitchLib/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,3 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "android.view", Managed="Android.View")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "android.content", Managed="Android.Content")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.bluebird.sled", Managed="Com.Bluebird.Sled")]
